8453183ef272018c279a10eef424e374b76c8930
Scott Mittman <sm2884@nau.edu>
2021-03-10 07:00:30 -0600

    hook up to deployed server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    //API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://race-director.herokuapp.com/api',
     TOKEN_KEY: 'blogful-client-auth-token',
   }
 
